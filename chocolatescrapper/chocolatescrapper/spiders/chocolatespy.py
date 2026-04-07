dea29caf7b043bd2e955ebc80dce53c8f8edcd35
Hassan1566 <sh456700992sj@gmail.com>
2026-04-07 11:52:19 +0500

    Update start_urls in ChocolatespySpider to point to the correct collection page

--- chocolatescrapper/chocolatescrapper/spiders/chocolatespy.py
+++ chocolatescrapper/chocolatescrapper/spiders/chocolatespy.py
@@ -4,7 +4,7 @@ import scrapy
 class ChocolatespySpider(scrapy.Spider):
     name = "chocolatespy"
     allowed_domains = ["chocolate.co.uk"]
-    start_urls = ["https://chocolate.co.uk"]
+    start_urls = ["https://www.chocolate.co.uk/collections/all"]
 
     def parse(self, response):
         pass
