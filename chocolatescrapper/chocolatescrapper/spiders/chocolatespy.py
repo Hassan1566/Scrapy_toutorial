bede5899c9b1994c705832b41ee5cb46f34dadf8
Hassan1566 <sh456700992sj@gmail.com>
2026-04-07 20:03:39 +0500

    Implement product parsing in ChocolatespySpider and add initial CSV data file

--- chocolatescrapper/chocolatescrapper/spiders/chocolatespy.py
+++ chocolatescrapper/chocolatescrapper/spiders/chocolatespy.py
@@ -7,4 +7,10 @@ class ChocolatespySpider(scrapy.Spider):
     start_urls = ["https://www.chocolate.co.uk/collections/all"]
 
     def parse(self, response):
-        pass
+        products = response.css("div.product-item")
+        for product in products:
+            yield {
+                'name': product.css("a.product-item-meta__title::text").get(),
+                'price': product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>',"").replace('</span>',""),
+                'url': product.css('div.product-item-meta a').attrib['href'],
+            }
