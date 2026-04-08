5a3a92c6fed7cfe077f1df2019f5c2a28a9158c7
Hassan1566 <sh456700992sj@gmail.com>
2026-04-08 22:23:53 +0500

    Add MedyScrap JSON data and implement product parsing in WebspySpider

--- WebScrapper/WebScrapper/spiders/Webspy.py
+++ WebScrapper/WebScrapper/spiders/Webspy.py
@@ -1,10 +1,19 @@
+from itertools import product
+from unicodedata import name
+
 import scrapy
 
 
 class WebspySpider(scrapy.Spider):
     name = "Webspy"
     allowed_domains = ["www.drogueriascolsubsidio.com"]
-    start_urls = ["https://www.drogueriascolsubsidio.com"]
+    start_urls = ["https://www.drogueriascolsubsidio.com/medicamentos"]
 
     def parse(self, response):
-        pass
+        products = response.css("div.ProductItemCard__info")
+        for product in products:
+            yield {
+                'name': product.css('div.ProductItemCard__info--wrap__title.pic::text').get(),
+                'price': product.css('div.ProductItemCard__info--wrap__prices').get().replace('<div class="ProductItemCard__info--wrap__prices"><div class="ProductItemCard__info--wrap__prices--inner"><div>$\xa0120.400</div><div>$\xa0',"").replace('</div><div>',"").replace('</div></div></div>',""),
+            
+            }
