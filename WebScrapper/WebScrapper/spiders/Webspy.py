dd08bee1c3c8311551728694af8e1b607a5b963c
Hassan1566 <sh456700992sj@gmail.com>
2026-04-08 22:30:42 +0500

    Update MedyScrap JSON data and enhance WebspySpider to include product URLs

--- WebScrapper/WebScrapper/spiders/Webspy.py
+++ WebScrapper/WebScrapper/spiders/Webspy.py
@@ -15,5 +15,5 @@ class WebspySpider(scrapy.Spider):
             yield {
                 'name': product.css('div.ProductItemCard__info--wrap__title.pic::text').get(),
                 'price': product.css('div.ProductItemCard__info--wrap__prices').get().replace('<div class="ProductItemCard__info--wrap__prices"><div class="ProductItemCard__info--wrap__prices--inner"><div>$\xa0120.400</div><div>$\xa0',"").replace('</div><div>',"").replace('</div></div></div>',""),
-            
+                
             }
