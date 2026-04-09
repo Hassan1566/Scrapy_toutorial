4b45c5faa869b01808a7e418e0fb02a029c6cb01
Hassan1566 <sh456700992sj@gmail.com>
2026-04-09 12:34:55 +0500

    Add MedyScrap CSV and JSON data with product details; enhance WebspySpider to extract product names, prices, and URLs

--- WebScrapper/WebScrapper/spiders/Webspy.py
+++ WebScrapper/WebScrapper/spiders/Webspy.py
@@ -1,7 +1,12 @@
-from itertools import product
-from unicodedata import name
-
 import scrapy
+
+
+class WebspySpider(scrapy.Spider):
+    name = "Webspy"
+    allowed_domains = ["www.drogueriascolsubsidio.com"]
+    start_urls = ["https://www.drogueriascolsubsidio.com/medicamentos"]
+
+    import scrapy
 
 
 class WebspySpider(scrapy.Spider):
@@ -11,9 +16,16 @@ class WebspySpider(scrapy.Spider):
 
     def parse(self, response):
         products = response.css("div.ProductItemCard__info")
+
         for product in products:
+            name = product.css('div.ProductItemCard__info--wrap__title.pic::text').get()
+            price = product.css('div.ProductItemCard__info--wrap__prices--inner div::text').getall()
+            price = [p.strip().replace('\xa0', ' ') for p in price]
+            url = product.xpath('./ancestor::a/@href').get()
+            url = response.urljoin(url)
+
             yield {
-                'name': product.css('div.ProductItemCard__info--wrap__title.pic::text').get(),
-                'price': product.css('div.ProductItemCard__info--wrap__prices').get().replace('<div class="ProductItemCard__info--wrap__prices"><div class="ProductItemCard__info--wrap__prices--inner"><div>$\xa0120.400</div><div>$\xa0',"").replace('</div><div>',"").replace('</div></div></div>',""),
-                
+                'name': name,
+                'price': price,
+                'url': url
             }
