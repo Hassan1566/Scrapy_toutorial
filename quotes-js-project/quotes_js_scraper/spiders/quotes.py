cab8b1d6a3363ec7b9d7030be6d35221b78339b0
Hassan1566 <sh456700992sj@gmail.com>
2026-04-21 14:09:09 +0500

    feat: initialize Scrapy project and implement Playwright-based quotes spider

--- quotes-js-project/quotes_js_scraper/spiders/quotes.py
+++ quotes-js-project/quotes_js_scraper/spiders/quotes.py
@@ -4,8 +4,33 @@ from quotes_js_scraper.items import QuoteItem
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+     
+    def start_requests(self):
+        url = "https://quotes.toscrape.com"
+        yield scrapy.Request(url=url, meta={"playwright":True, 
+                                        "playwright_include_page":True,
+                                        "errback":self.errback})
 
-    def parse(self, response):
-        pass
+
+    async def parse(self, response):
+        page = response.meta['playwright_page']
+        self.logger.info(f"Scraping: {response.url}")
+        await page.wait_for_selector("div.quote")
+        
+        
+        
+        
+        
+        for q in response.css('div.quote'):
+            quote = QuoteItem()
+            quote['text'] = q.css('span.text::text').get()
+            quote['author'] = q.css('small.author::text').get()
+            quote['tags'] = q.css('div.tags a.tag::text').getall()
+            yield quote
+        
+        await page.close()
+
+    async def errback(self, failure):
+        page = failure.request.meta['playwright_page']
+        await page.close()
+        self.logger.error(repr(failure))
